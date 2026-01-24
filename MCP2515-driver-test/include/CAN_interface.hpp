5e21154563f6b56ec019c7a73f86e565abee2b72
danielk125 <158085907+danielk125@users.noreply.github.com>
2026-01-24 13:02:22 -0600

    can signal def

--- MCP2515-driver-test/include/CAN_interface.hpp
+++ MCP2515-driver-test/include/CAN_interface.hpp
@@ -1,6 +1,26 @@
 #include "ICAN.hpp"
 
+enum class SignalType {
+    UINT8,
+    UINT16,
+    UINT32,
+    INT8,
+    INT16,
+    INT32,
+    FLOAT
+};
 
+class CAN_Signal {
+public:
+    SignalType& s;
+    uint8_t _startBit;
+    uint8_t _length;
+    uint8_t _factor;
+    uint8_t _offset;
+
+    CAN_Signal(SignalType& signalType, uint8_t startBit, uint8_t length, uint8_t factor, uint8_t offset) :
+    s(signalType), _startBit(startBit), _length(length), _factor(factor), _offset(offset) {};
+};
 
 class CAN_Bus {
     ICAN& _can;
