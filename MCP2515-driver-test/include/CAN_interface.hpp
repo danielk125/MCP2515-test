781f60fc3bf5f87b6604e5c6dae10cf4c598032a
danielk125 <158085907+danielk125@users.noreply.github.com>
2026-01-26 21:52:58 -0600

    can interface work

--- MCP2515-driver-test/include/CAN_interface.hpp
+++ MCP2515-driver-test/include/CAN_interface.hpp
@@ -12,14 +12,22 @@ enum class SignalType {
 
 class CAN_Signal {
 public:
+    CAN_Signal(SignalType& signalType, uint8_t startBit, uint8_t length, uint8_t factor, uint8_t offset) :
+    s(signalType), _startBit(startBit), _length(length), _factor(factor), _offset(offset) {};
+
     SignalType& s;
     uint8_t _startBit;
     uint8_t _length;
     uint8_t _factor;
     uint8_t _offset;
+};
 
-    CAN_Signal(SignalType& signalType, uint8_t startBit, uint8_t length, uint8_t factor, uint8_t offset) :
-    s(signalType), _startBit(startBit), _length(length), _factor(factor), _offset(offset) {};
+template<size_t num_signals>
+class CAN_Message {
+public:
+    CAN_Message
+private:
+    std::array<CAN_Signal, num_signals> _signals;
 };
 
 class CAN_Bus {
