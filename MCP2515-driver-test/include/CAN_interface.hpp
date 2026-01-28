8b23dc9383e9cc18d9967fa72e98eba78477504b
Daniel Kramer <dakramer05@gmail.com>
2026-01-27 18:40:00 -0600

    more interface work

--- MCP2515-driver-test/include/CAN_interface.hpp
+++ MCP2515-driver-test/include/CAN_interface.hpp
@@ -1,4 +1,5 @@
 #include "ICAN.hpp"
+#include <variant>
 #include <span>
 
 enum class SignalType {
@@ -11,36 +12,66 @@ enum class SignalType {
     FLOAT
 };
 
+enum class Endianness {
+    littleEndian,
+    bigEndian
+}
+
 class CAN_Signal {
 public:
-    CAN_Signal(SignalType& signalType, uint8_t startBit, uint8_t length, uint8_t factor, uint8_t offset, bool isSigned) :
-    _s(signalType), _startBit(startBit), _length(length), _factor(factor), _offset(offset), _isSigned(isSigned) {};
+    CAN_Signal(SignalType signalType, uint8_t startBit, uint8_t length, double factor, double offset, bool isSigned, Endianness endian) :
+    _sType(signalType), _startBit(startBit), _length(length), _factor(factor), _offset(offset), _isSigned(isSigned), _endian(endian) {};
 
     SignalType s();
     uint8_t startBit();
     uint8_t length();
     uint8_t factor();
     uint8_t offset();
-    bool    isSigned();  
+    bool    isSigned(); 
 
-    void decode(std::span<const uint8_t> data) const;
+    template <class T>
+    T get() const { 
+        if (!std::holds_alternative<T>(_value)) {
+            static_assert(false && "CAN_Signal: invalid get<T>() for this signal's stored type");
+        }
+        return std::get<T>(_value); 
+    }
+
+    void decode(std::span<const uint8_t> data);
     void encode(std::span<uint8_t> data) const;
 
 private:
-    SignalType  _s;
+    using SignalValue = std::variant<uint8_t, uint16_t, uint32_t, int8_t,  int16_t,  int32_t, float>;
+
+    static SignalValue defaultValueFor(SignalType t) {
+        switch (t) {
+            case SignalType::UINT8:  return uint8_t{0};
+            case SignalType::UINT16: return uint16_t{0};
+            case SignalType::UINT32: return uint32_t{0};
+            case SignalType::INT8:   return int8_t{0};
+            case SignalType::INT16:  return int16_t{0};
+            case SignalType::INT32:  return int32_t{0};
+            case SignalType::FLOAT:  return float{0.0f};
+        }
+        return uint32_t{0};
+    }
+
+    void storePhysicalIntoValue(double phys);
+
+    SignalType  _sType;
     uint8_t     _startBit;
     uint8_t     _length;
-    uint8_t     _factor;
-    uint8_t     _offset;
+    double     _factor;
+    double     _offset;
     bool        _isSigned;
-
-
+    Endianness _endian;
+    SignalValue _sValue;
 };
 
 template<size_t num_signals>
 class CAN_Message {
 public:
-    CAN_Message(uint32_t id, uint8_t length, std::array<Signal, NumSignals> signals, bool extended = false)
+    CAN_Message(uint32_t id, bool extended, uint8_t length, std::array<Signal, NumSignals> signals);
 
     uint32_t id();
     uint8_t length();
